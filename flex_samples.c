bd329a84cd94136e29c0fb392473d0ea5d665add
Evgeny Lositsky <elosyash239@gmail.com>
2021-10-16 20:33:09 +0300

    added programs to repo

---
+++ flex_samples.c
@@ -0,0 +1,11 @@
+void flex_test() {
+	setPosSlowly(350, 100, 100, 5, 100);
+	setPosSlowly(100, 350, 100, 5, 100);
+	setPosSlowly(100, 100, 350, 5, 100);
+	setPosSlowly(350, 350, 350, 5, 100);
+
+	setPosSlowly(100, 100, 350, 5, 100);
+	setPosSlowly(100, 350, 100, 5, 100);
+	setPosSlowly(350, 100, 100, 5, 100);
+	setPosSlowly(350, 350, 350, 5, 100);
+}
